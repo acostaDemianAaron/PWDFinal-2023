34f8c7a11993f4398704bcecf58b34aef6a11f68
Cristian Vera <vera.alan2207@gmail.com>
2023-11-12 19:56:29 -0300

    Erased password of database

--- Model/Connection/Database.php
+++ Model/Connection/Database.php
@@ -26,8 +26,8 @@ class Database extends PDO
         $this->engine = "mysql";
         $this->host = "localhost";
         $this->database = "bdcarritocompras";
-        $this->user = "root"; // Agregar usuario para ingresar a la base de datos
-        $this->pass = "191199"; // Agregar contraseña para ingresar a la base de datos.
+        $this->user = ""; // Agregar usuario para ingresar a la base de datos
+        $this->pass = ""; // Agregar contraseña para ingresar a la base de datos.
         $this->debug = true;
         $this->error = "";
         $this->query = "";
