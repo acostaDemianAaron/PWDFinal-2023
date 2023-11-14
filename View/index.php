6bde55aae760b78b1d9b3798f8e4aaf79aeff16d
Cristian Vera <vera.alan2207@gmail.com>
2023-11-14 02:05:52 -0300

    Index to visualize session

--- View/index.php
+++ View/index.php
@@ -1,8 +1,20 @@
 <?php
 include_once("../Config/config.php");
 // TODO header call
-new Header("Titulo", $DIRS, 0);
+new Header("Titulo", $DIRS);
+
+print_r($_SESSION);
 
 // TODO contents
-
+if(array_key_exists('msg', data_submitted())){
+   if(data_submitted()['msg'] == 'logout'){
+      echo <<<HTML
+      <h1>Successful Log out!</h1>
+      HTML;
+   }
+} else if(null !== session_id()){
+   echo <<<HTML
+      <h1>Logged In!</h1>
+      HTML;
+}
 // TODO footer call
