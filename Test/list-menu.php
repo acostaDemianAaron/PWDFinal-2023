95488fad073c1d07fb606b692981d27dd9cde007
Cristian Vera <vera.alan2207@gmail.com>
2023-11-14 20:19:04 -0300

    Test

--- Test/list-menu.php
+++ Test/list-menu.php
@@ -1,23 +1,23 @@
 <?php
 include_once("../Config/config.php");
-/**
- * Convert Object to associative Array.
- * Uses every 'get' method and uses its name to create the key associated.
- * @param Object $object Object of any type that has 'get' methods.
- * @return array $array Object as associative Array.
- */
-function ObjectToArray($object)
-{
-   $array = [];
-   $methodsArray = get_class_methods($object);
-   foreach ($methodsArray as $method) {
-      if (mb_substr($method, 0, 3) == "get" && mb_substr($method, 3, 3) != "Obj") {
-         $key = strtolower(mb_substr($method, 3));
-         $array[$key] = $object->$method();
-      }
-    }
-    return $array; // For now echo.
-}
+// /**
+//  * Convert Object to associative Array.
+//  * Uses every 'get' method and uses its name to create the key associated.
+//  * @param Object $object Object of any type that has 'get' methods.
+//  * @return array $array Object as associative Array.
+//  */
+// function ObjectToArray($object)
+// {
+//    $array = [];
+//    $methodsArray = get_class_methods($object);
+//    foreach ($methodsArray as $method) {
+//       if (mb_substr($method, 0, 3) == "get" && mb_substr($method, 3, 3) != "Obj") {
+//          $key = strtolower(mb_substr($method, 3));
+//          $array[$key] = $object->$method();
+//       }
+//     }
+//     return $array; // For now echo.
+// }
 
 if(data_submitted()['type']){
    // Create the object of the respective class.
