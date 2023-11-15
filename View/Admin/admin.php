0762014505b51393e1d641a03f6a3f24d0e7b837
acostaDemianAaron <acostademiann14@gmail.com>
2023-11-15 18:53:30 -0300

    ...

--- View/Admin/admin.php
+++ View/Admin/admin.php
@@ -28,20 +28,42 @@ echo <<<HTML
 
 
 
-<div id="dlg" class="easyui-dialog" style="width:400px" data-options="closed:true,modal:true,border:'thin',buttons:'#dlg-buttons'">
-      <form id="fm" method="post" novalidate style="margin:0;padding:20px 50px">
-         <h3>User Information</h3>
-         <div style="margin-bottom:10px">
-            <input name="firstname" class="easyui-textbox" required="true" label="First Name:" style="width:100%">
+<!--Crear nuevo usuario-->
+<div id="dlg" class="easyui-dialog" style="width:400px;height:280px;padding:10px 20px" closed="true" buttons="#dlg-buttons">
+      <div class="ftitle">User Information</div>
+      <form id="fm" method="post" novalidate>
+         <div class="fitem">
+            <label>Name:</label>
+            <input name="usnombre" class="easyui-textbox" required="true">
          </div>
-         <div style="margin-bottom:10px">
-            <input name="lastname" class="easyui-textbox" required="true" label="Last Name:" style="width:100%">
+         <div class="fitem">
+            <label>Password:</label>
+            <input name="uspass" class="easyui-textbox" required="true">
          </div>
-         <div style="margin-bottom:10px">
-            <input name="phone" class="easyui-textbox" required="true" label="Phone:" style="width:100%">
+         <div class="fitem">
+            <label>Mail:</label>
+            <input name="usmail" class="easyui-textbox" validType="email">
          </div>
-         <div style="margin-bottom:10px">
-            <input name="email" class="easyui-textbox" required="true" validType="email" label="Email:" style="width:100%">
+         <div class="fitem">
+            <label class="form-check-label">Rol</label>
+            <div class="form-check">
+               <input class="form-check-input" type="radio" name="idrol" id="1">
+               <label class="form-check-label" for="1">
+                  Admin
+               </label>
+            </div>
+            <div class="form-check">
+               <input class="form-check-input" type="radio" name="idrol" id="2">
+               <label class="form-check-label" for="2">
+                  Deposito
+               </label>
+            </div>
+            <div class="form-check">
+               <input class="form-check-input" type="radio" name="idrol" id="3" checked>
+               <label class="form-check-label" for="3">
+                  Usuario
+               </label>
+            </div>
          </div>
       </form>
    </div>
@@ -49,66 +71,33 @@ echo <<<HTML
       <a href="javascript:void(0)" class="easyui-linkbutton c6" iconCls="icon-ok" onclick="saveUser()" style="width:90px">Save</a>
       <a href="javascript:void(0)" class="easyui-linkbutton" iconCls="icon-cancel" onclick="javascript:$('#dlg').dialog('close')" style="width:90px">Cancel</a>
    </div>
-   <script type="text/javascript">
-      var url;
-
+   <script>
       function newUser() {
-         $('#dlg').dialog('open').dialog('center').dialog('setTitle', 'New User');
+         $('#dlg').dialog('open').dialog('setTitle', 'New User');
          $('#fm').form('clear');
          url = 'save_user.php';
       }
-
-      function editUser() {
-         var row = $('#dg').datagrid('getSelected');
-         if (row) {
-            $('#dlg').dialog('open').dialog('center').dialog('setTitle', 'Edit User');
-            $('#fm').form('load', row);
-            url = 'update_user.php?id=' + row.id;
-         }
-      }
-
-      function saveUser() {
-         $('#fm').form('submit', {
-            url: url,
-            iframe: false,
-            onSubmit: function() {
-               return $(this).form('validate');
-            },
-            success: function(result) {
-               var result = eval('(' + result + ')');
-               if (result.errorMsg) {
-                  $.messager.show({
-                     title: 'Error',
-                     msg: result.errorMsg
-                  });
-               } else {
-                  $('#dlg').dialog('close'); // close the dialog
-                  $('#dg').datagrid('reload'); // reload the user data
-               }
-            }
-         });
-      }
-
-      function destroyUser() {
-         var row = $('#dg').datagrid('getSelected');
-         if (row) {
-            $.messager.confirm('Confirm', 'Are you sure you want to destroy this user?', function(r) {
-               if (r) {
-                  $.post('destroy_user.php', {
-                     id: row.id
-                  }, function(result) {
-                     if (result.success) {
-                        $('#dg').datagrid('reload'); // reload the user data
-                     } else {
-                        $.messager.show({ // show error message
-                           title: 'Error',
-                           msg: result.errorMsg
-                        });
-                     }
-                  }, 'json');
-               }
-            });
-         }
-      }
    </script>
 HTML;
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Document</title>
+</head>
+
+<body>
+
+
+
+
+
+
+</body>
+
+</html>
