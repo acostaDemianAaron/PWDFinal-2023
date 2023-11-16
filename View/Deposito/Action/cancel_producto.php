5afba8b95da8943a78308e046fa69f41fe2fce13
Santiago Yaitul <santiago.yaitul@yahoo.com>
2023-11-16 04:26:06 -0300

    Cambios add Cancel_product

--- View/Deposito/Action/cancel_producto.php
+++ View/Deposito/Action/cancel_producto.php
@@ -2,5 +2,23 @@
 include_once('../../../Config/config.php');
 
 $data = data_submitted();
-print_r($data);
-die();
+
+$compraEstado = new ABMCompraEstado();
+$data['cefechafin'] = "NULL";
+$objCompraEstado = $compraEstado->Search($data)[0];
+if ($ultimoEstado != 4) {
+    $data['cefechafin'] = date("Y-m-d H:i:s");
+    $data['idcompraestado'] = $objCompraEstado->getIdCompraEstado();
+    $compraEstado->Edit($data);
+
+    $data['cefechaini'] = $data['cefechafin'];
+    unset($data['cefechafin']);
+    unset($data['idcompraestado']);
+
+    $data['idcompraestadotipo'] = 4;
+    $compraEstado->Add($data);
+    $res['success'] = TRUE;
+} else {
+    $res['errorMsg'] = "no se puede procesar el pedido";
+}
+echo json_encode($res);
